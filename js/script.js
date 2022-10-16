82e0537d1cacb17915ad44ae278e2480b3b0a80b
Carmen-Rohr <114531891+Carmen-Rohr@users.noreply.github.com>
2022-10-15 22:19:36 -0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+const music = new Audio('./sonidos/error.wav');
+
+const botones = document.querySelectorAll(".boton-teclado");
+// Definir función y evitar definirla de manera anónima
+const cuandoSeHaceClick = function (evento) {
+	
+	if (texto.textContent.length==1 && this.innerText=="S") {
+	  	texto.textContent= texto.textContent+this.innerText;
+		}
+		else if (texto.textContent.length==1 && this.innerText !="S") {
+			texto.textContent= texto.textContent+this.innerText;			
+			music.play();
+		} else if (texto.textContent.length==2 && this.innerText=="O") {
+		texto.textContent= texto.textContent+this.innerText;
+		}
+	    else if (texto.textContent.length==2 && this.innerText !="O") {
+	   	    texto.textContent= texto.textContent+this.innerText;			
+			music.play();
+		} else if (texto.textContent.length==3 && this.innerText=="L") {
+			texto.textContent= texto.textContent+this.innerText;
+			const sol = new Audio('./sonidos/sol.ogg');
+			sol.play();
+			setTimeout(function(){
+				const modal= document.getElementById('ganaste');
+				const close = document.getElementById('close');
+				const sonido = new Audio('./sonidos/ganaste.wav');
+
+				
+				
+				function mostrar2() {
+					div = document.getElementById('siguiente');
+					div.style.display = '';
+				};
+
+				mostrar2();
+
+				modal.setAttribute('open', 'true');
+				sonido.play();
+				close.addEventListener('click',() => { 
+					modal.removeAttribute('open')})
+					sonido.stop();
+			},1000);
+			
+		}
+			else if (texto.textContent.length==3 && this.innerText !="L") {
+				texto.textContent= texto.textContent+this.innerText;			
+				music.play();
+			}	
+	}
+
+
+
+function clicked(){
+	var palabra;
+	if (texto.textContent.length==1) {
+		console.log(texto.textContent);
+		texto.textContent="";	
+		console.log("texto.textcontent "+ texto.textContent);	
+	} else {
+		texto.textContent = texto.textContent.substring(0, texto.textContent.length - 1);
+		console.log(texto.textContent + "longitud "+ texto.textContent.length);
+	}
+}
+
+// botones es un arreglo así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", cuandoSeHaceClick);
+});
+
+//document.getElementById("texto").innerHTML = this.innerText;
---
+++ js/script4.js
@@ -0,0 +1,80 @@
+const music = new Audio('./sonidos/error.wav');
+
+const botones = document.querySelectorAll(".boton-teclado");
+
+/*div = document.getElementById('estrella1');
+div.style.display = 'none';
+div = document.getElementById('estrella2');
+div.style.display = '';*/
+
+// Definir función y evitar definirla de manera anónima
+const cuandoSeHaceClick = function (evento) {
+	
+	if (texto.textContent.length==1 && this.innerText=="P") {
+	  	texto.textContent= texto.textContent+this.innerText;
+		}
+		else if (texto.textContent.length==1 && this.innerText !="P") {
+			texto.textContent= texto.textContent+this.innerText;			
+			music.play();
+		} else if (texto.textContent.length==2 && this.innerText=="A") {
+		texto.textContent= texto.textContent+this.innerText;
+		}
+	    else if (texto.textContent.length==2 && this.innerText !="A") {
+	   	    texto.textContent= texto.textContent+this.innerText;			
+			music.play();
+		} else if (texto.textContent.length==3 && this.innerText=="P") {
+			texto.textContent= texto.textContent+this.innerText;
+			}
+			else if (texto.textContent.length==3 && this.innerText !="P") {
+				   texto.textContent= texto.textContent+this.innerText;			
+				music.play();	
+		} else if (texto.textContent.length==4 && this.innerText=="A") {
+			texto.textContent= texto.textContent+this.innerText;
+			const papa = new Audio('./sonidos/papa.ogg');
+			papa.play();
+			setTimeout(function(){
+				const modal= document.getElementById('ganaste');
+				const close = document.getElementById('close');
+				const sonido = new Audio('./sonidos/ganaste.wav');
+					
+				function mostrar2() {
+					div = document.getElementById('siguiente');
+					div.style.display = '';
+				};
+
+				mostrar2();
+
+				modal.setAttribute('open', 'true');
+				sonido.play();
+				close.addEventListener('click',() => { 
+					modal.removeAttribute('open')})
+					sonido.stop();
+			},1000);
+			} else if (texto.textContent.length==4 && this.innerText !="A") {
+				texto.textContent= texto.textContent+this.innerText;			
+				music.play();
+			}	
+				
+	}
+
+
+
+function clicked(){
+	var palabra;
+	if (texto.textContent.length==1) {
+		console.log(texto.textContent);
+		texto.textContent="";	
+		console.log("texto.textcontent "+ texto.textContent);	
+	} else {
+		texto.textContent = texto.textContent.substring(0, texto.textContent.length - 1);
+		console.log(texto.textContent + "longitud "+ texto.textContent.length);
+	}
+}
+
+// botones es un arreglo así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", cuandoSeHaceClick);
+});
+
+//document.getElementById("texto").innerHTML = this.innerText;
